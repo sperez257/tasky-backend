a9040dd12bf97dd02d0168a0a6eee03f4cda0c57
Sebastián Pérez Chaparro <sperezc5@upao.edu.pe>
2023-10-27 23:18:56 -0500

    primer commit

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectToDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://omkhetwal:qfxoQkUzbzQWaoMo@youtube.lothdki.mongodb.net/blossom-app?retryWrites=true&w=majority"
+      "mongodb+srv://sperezc5:DuJnd31AQePez7P0@blossom-app.lggzkl6.mongodb.net/?retryWrites=true&w=majority"
     )
     if (connection) {
       console.log("Connection established")
